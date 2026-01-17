4c5741bc3fe61ec049dc3280a2670746266ef5b8
l1l <khalil23ar@gmail.com>
2026-01-17 23:56:23 +0100

    feat(tracking): capture mousemove and scroll events

--- event-tracker/main.js
+++ event-tracker/main.js
@@ -1,5 +1,13 @@
 "use strict";
 
+function addEvent(ele, e, fn) {
+  return ele.addEventListener(e, fn);
+}
+
+function selectA(cssClass) {
+  return document.querySelectorAll(cssClass);
+}
+
 const userEnv = {
   appVersion: navigator.appVersion,
   browser: navigator.userAgentData?.brands?.[0]?.brand,
@@ -12,3 +20,40 @@ const userEnv = {
   screenSize: `${screen.width}px x ${screen.height}px`,
   timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 };
+
+const record = {
+  userEnv: userEnv,
+  mouseCoords: [],
+  scroll: [],
+};
+
+const momentousE = ["mousemove", "scroll"];
+
+const fns = [
+  function addMouseCoords(e) {
+    let t = Date.now();
+    record.mouseCoords.push({ x: e.clientX, y: e.clientY, t: t });
+    console.log(record);
+  },
+
+  function addScrollCoords() {
+    let date = new Date();
+    record.scroll.push({ x: window.scrollX, y: window.scrollY, t: date });
+    console.log(record);
+  },
+];
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function (e) {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(e);
+    }, delay);
+  };
+}
+
+for (let i = 0; i < momentousE.length; i++) {
+  addEvent(document, momentousE[i], debounce(fns[i], 100));
+}
