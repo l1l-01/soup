6e127483fe82d1716a72035902b04ae08c59b4c1
l1l <khalil23ar@gmail.com>
2026-01-18 19:45:22 +0100

    feat(server): bootstrap minimal Axum application with root endpoint

--- event-processor/src/main.rs
+++ event-processor/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/",get(|| async {"Event Processor!"}));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener,app).await.unwrap();
 }
